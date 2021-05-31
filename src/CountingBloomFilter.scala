d5904cd38359864b46292a9aa19eba421b7c7ed0
Alicja K <alicja.karlowicz@gmail.com>
2021-05-31 11:53:53 +0200

    small fixes

--- src/CountingBloomFilter.scala
+++ src/CountingBloomFilter.scala
@@ -12,28 +12,34 @@ object MainCBF {
 
     val arg = emp1 #:: emp2 #:: emp3 #:: emp4 #:: LazyList.empty
 
-    val Filter = new CountingBloomFilter(arg, 100, 10)
+    val cbf = new CountingBloomFilter(arg, 100, 10)
 
     println("Does James Bond work here?")
-    Filter.check(emp5)
+    println(cbf.check(emp5))
     println("Let's hire James Bond")
-    Filter.add(emp5)
+    cbf.add(emp5)
     println("Does James Bond work here?")
-    Filter.check(emp5)
+    println(cbf.check(emp5))
     println("Does John Doe work here?")
-    Filter.check(emp1)
+    println(cbf.check(emp1))
     println("Firing John Doe")
-    Filter.delete(emp1)
+    cbf.delete(emp1)
     println("Does John Doe work here?")
-    Filter.check(emp1)
+    println(cbf.check(emp1))
   }
 
 }
 
 
 class CountingBloomFilter(s: LazyList[Employee], m: Int, k: Int) {
+
+  require(s.size > 0, "Empty stream")
+  require(m > 0, "Number of counters must be grater than zero")
+  require(k > 0, "Number of hash function must be grater than zero")
+
   var buffer: Array[Int] = new Array[Int](m)
 
+  // initialize counters to zero
   for (i <- 0 until m)
     buffer(i) = 0
 
@@ -41,30 +47,32 @@ class CountingBloomFilter(s: LazyList[Employee], m: Int, k: Int) {
   s.foreach {
     x => {
       for (i <- 0 until k)
-        buffer((MurmurHash3.stringHash(x.toString, i) % m).abs) += 1
-
+          buffer((MurmurHash3.stringHash(x.toString, i) % m).abs) += 1 //add counter
     }
   }
 
+
+  // add element to filter
   def add(x: Employee): Unit = {
     for (i <- 0 until k)
       buffer((MurmurHash3.stringHash(x.toString, i) % m).abs) += 1
   }
 
+  //remove element from filter
   def delete(x: Employee): Unit = {
     for (i <- 0 until k)
-      buffer((MurmurHash3.stringHash(x.toString, i) % m).abs) -= 1
+      buffer((MurmurHash3.stringHash(x.toString, i) % m).abs) -= 1 //decrement counter
   }
 
-  def check(x: Employee): Unit = {
+  // check for element
+  def check(x: Employee): Boolean = {
 
-    var notFound: Boolean = false
+    var found : Boolean = true
 
     for (i <- 0 until k)
-      if (buffer((MurmurHash3.stringHash(x.toString, i) % m).abs) == 0) notFound = true
+      if (buffer((MurmurHash3.stringHash(x.toString, i) % m).abs) == 0) found = false
 
-    if (notFound) println("The employee does not work here")
-    else println("The employee works here")
+    found
   }
 
 }
